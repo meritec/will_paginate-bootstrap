6d9834c0cfe5151b966818f6f308a969dd14297c
Jordan Green <jordan.green@cpoms.co.uk>
2018-06-25 14:14:10 +0100

    Allow pass through of custom HTML attributes

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -15,7 +15,7 @@ module BootstrapPagination
         end
       end.join(@options[:link_separator])
 
-      tag("ul", list_items, class: ul_class)
+      tag("ul", list_items, container_attributes.merge(class: ul_class))
     end
 
     def container_attributes
